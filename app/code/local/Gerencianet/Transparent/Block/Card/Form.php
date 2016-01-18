5e095a97d0eb6937de946c7591f34d582437ea9a
Thomaz Feitoza <thomazfeitoza@gmail.com>
2016-01-18 17:19:34 -0200

    Corrige gravação de cartão de crédito

--- app/code/local/Gerencianet/Transparent/Block/Card/Form.php
+++ app/code/local/Gerencianet/Transparent/Block/Card/Form.php
@@ -20,16 +20,16 @@ class Gerencianet_Transparent_Block_Card_Form extends Mage_Payment_Block_Form_Cc
      * Block constructor
      */
     protected function _construct() {
-    	parent::_construct();
+      parent::_construct();
         $this->setTemplate('gerencianet/card/form.phtml');
     }
 
     /**
      * Returns card types from source model
      */
-	public function getSourceModel()
+  public function getSourceModel()
     {
-		return Mage::getSingleton('gerencianet_transparent/source_cards');
+    return Mage::getSingleton('gerencianet_transparent/source_cards');
     }
 
     /**
@@ -48,158 +48,212 @@ class Gerencianet_Transparent_Block_Card_Form extends Mage_Payment_Block_Form_Cc
 
         return $return;
     }
-    
+
     /**
      * Prepares layout for show on screen
      */
     protected function _prepareLayout(){
-    	$testmode = Mage::helper('gerencianet_transparent')->isSandbox();
-    	$account_id = Mage::getStoreConfig('payment/gerencianet_transparent/account_id');
-    	// add Gerencianet JS after <body>
-    	$jsBlock = Mage::app()->getLayout()->createBlock('core/text', 'js_gerencianet');
-    	$jsBlock->setText("<script type='text/javascript'>
-    		//<![CDATA[
-			var brand = '';
+      $testmode = Mage::helper('gerencianet_transparent')->isSandbox();
+      $account_id = Mage::getStoreConfig('payment/gerencianet_transparent/account_id');
+      // add Gerencianet JS after <body>
+      $jsBlock = Mage::app()->getLayout()->createBlock('core/text', 'js_gerencianet');
+      $jsBlock->setText("<script type='text/javascript'>
+        //<![CDATA[
+      var brand = '';
 
-			function onlyNumbers(evt){
-				var e = event || evt; // for trans-browser compatibility
-				var charCode = e.which || e.keyCode;
-				if (charCode > 31 && (charCode < 48 || charCode > 57))
-					return false;
-				return true;
-			}
+      function onlyNumbers(evt){
+        var e = event || evt; // for trans-browser compatibility
+        var charCode = e.which || e.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57))
+          return false;
+        return true;
+      }
 
-			function checkToken() {
-    	        token = document.getElementById('gerencianet_card_token').value;
-    	        if(!token) {
-					var cards = document.getElementsByName('payment[cc_type]');
-    				for (i = 0; i < cards.length; i++) {
-    					if (cards[i].checked) {
-    						var type = cards[i].value;
-    						if (cards[i].value === 'amex') {
-    							document.getElementById('gerencianet_card_cc_cid').setAttribute('maxlength', 4);
-    						} else {
-    							document.getElementById('gerencianet_card_cc_cid').setAttribute('maxlength', 3);
-    						}
-        	    		}
-    				}
-    				var number		= document.getElementById('gerencianet_card_cc_number').value;
-    				var cvv			= document.getElementById('gerencianet_card_cc_cid').value;
-    				var exp_month	= document.getElementById('gerencianet_card_cc_expiration').value;
-    				var exp_year	= document.getElementById('gerencianet_card_cc_expiration_yr').value;
-    		
-    				if (!type) {
-    					return false;
-    				}
-    				
-    				if (!number) {
-    					return false;
-    				}
-    				if (!cvv) {
-    					return false;
-    				}
-    				if (!exp_month) {
-    					return false;
-    				}
-    				if (!exp_year) {
-    					return false;
-    				}
-        	        
-        	        var callback = function(error, response) {
-    					if(error) {
-    					  //console.error(error);
-    					} else {
-    					  document.getElementById('gerencianet_card_token').value = response.data.payment_token;
-    					  //console.log(response);
-    					}
-    				};
-    		
-    				$"."gn.checkout.getPaymentToken({
-    					brand: type,
-    					number: number,
-    					cvv: cvv,
-    					expiration_month: exp_month,
-    					expiration_year: exp_year
-    				}, callback);
-    	       }
-			}
+      function checkToken() {
+              token = document.getElementById('gerencianet_card_token').value;
+              if(!token) {
+          var cards = document.getElementsByName('payment[cc_type]');
+            for (i = 0; i < cards.length; i++) {
+              if (cards[i].checked) {
+                var type = cards[i].value;
+                if (cards[i].value === 'amex') {
+                  document.getElementById('gerencianet_card_cc_cid').setAttribute('maxlength', 4);
+                } else {
+                  document.getElementById('gerencianet_card_cc_cid').setAttribute('maxlength', 3);
+                }
+                  }
+            }
+            var number      = document.getElementById('gerencianet_card_cc_number').value;
+            var cvv         = document.getElementById('gerencianet_card_cc_cid').value;
+            var exp_month   = document.getElementById('gerencianet_card_cc_expiration').value;
+            var exp_year    = document.getElementById('gerencianet_card_cc_expiration_yr').value;
 
-			function calculateInstallments(newBrand) {
-		        if(brand != newBrand || document.getElementById('gerencianet_card_cc_installments').length == 0) {
-		            brand = newBrand;
-    	            document.getElementById('gerencianet_card_token').value = '';
-		            var urlAjax = '" . Mage::getUrl("gerencianet/payment/installments") . "';
-        	        
-    	            if (newBrand === 'aura') {
-    		  		    document.getElementById('gerencianet_card_cc_number').setAttribute('maxlength', 19);
-    				} else {
-    				    document.getElementById('gerencianet_card_cc_number').setAttribute('maxlength', 16);
-    				}
-    		
-    		        new Ajax.Request( urlAjax, {
-    		                method: 'POST',
-    		                parameters: 'brand=' + brand,
-    		                evalScripts: true,
-    		                onLoading: function(transport) {
-    		                        $('installments').innerHTML = '<img src=\"".$this->getSkinUrl('images/opc-ajax-loader.gif')."\" /> <span style=\"color: #888;\">Carregando Parcelas...</span>';
-    		                },
-    		                onSuccess: function(transport) {
-    	                        if (200 == transport.status) {
-    	                        	$('installments').innerHTML = transport.responseText;
-    	                        }
-    						},
-    		                onFailure: function() {}
-    		        });
-		        }
-			}
-    	    
-    	    function checkNewToken() {
-    	        token = document.getElementById('gerencianet_card_token');
-    	        if (token) {
-        	        if(token.value) {
-            	        var urlAjax = '" . Mage::getUrl("gerencianet/payment/checktoken") . "';
-            	        new Ajax.Request( urlAjax, {
-        		                method: 'POST',
-        		                parameters: 'token=' + token.value,
-        		                evalScripts: true,
-        		                onSuccess: function(transport) {
-        	                        if (200 == transport.status) {
-            	                       if (transport.responseText == 'FALSE') {
-        	                        	  document.getElementById('gerencianet_card_token').value = '';
-        	                              checkToken();
-        	                           }
-        	                        }
-        						},
-        		                onFailure: function() {}
-        		        });
-        	        }
-    	       }
-    	    }
-		
-			window.onload = function(){
-				var s=document.createElement('script');
-				s.type='text/javascript';
-				var v=parseInt(Math.random()*1000000);
-				s.src='https://".( ($testmode) ? 'sandbox' : 'api' ) . ".gerencianet.com.br/v1/cdn/" . $account_id . "/'+v;
-				s.async=false;
-				s.id='". $account_id . "';
-				if(!document.getElementById('" . $account_id . "')){
-					document.getElementsByTagName('head')[0].appendChild(s);
-				};
-    			$"."gn={validForm:true,processed:false,done:{},ready:function(fn){ $"."gn.done=fn; }};
-				$"."gn.ready(function(checkout) {});
-    	        setInterval(checkNewToken, 4000);
-			};
-		//]]>
-		</script>");
-    			
-    	$head = Mage::app()->getLayout()->getBlock('after_body_start');
-    
-    	if($head) {
-    		$head->append($jsBlock);
-    	}
-    
-    	return parent::_prepareLayout();
+            if (!type) {
+              return false;
+            }
+
+            if (!number) {
+              return false;
+            }
+            if (!cvv) {
+              return false;
+            }
+            if (!exp_month) {
+              return false;
+            }
+            if (!exp_year) {
+              return false;
+            }
+
+                  var callback = function(error, response) {
+              if(error) {
+                //console.error(error);
+              } else {
+                document.getElementById('gerencianet_card_token').value = response.data.payment_token;
+                //console.log(response);
+              }
+            };
+
+            $"."gn.checkout.getPaymentToken({
+              brand: type,
+              number: number,
+              cvv: cvv,
+              expiration_month: exp_month,
+              expiration_year: exp_year
+            }, callback);
+             }
+      }
+
+      function calculateInstallments(newBrand) {
+            if(brand != newBrand || document.getElementById('gerencianet_card_cc_installments').length == 0) {
+                brand = newBrand;
+                  document.getElementById('gerencianet_card_token').value = '';
+                var urlAjax = '" . Mage::getUrl("gerencianet/payment/installments") . "';
+
+                  if (newBrand === 'aura') {
+                  document.getElementById('gerencianet_card_cc_number').setAttribute('maxlength', 19);
+            } else {
+                document.getElementById('gerencianet_card_cc_number').setAttribute('maxlength', 16);
+            }
+
+                new Ajax.Request( urlAjax, {
+                        method: 'POST',
+                        parameters: 'brand=' + brand,
+                        evalScripts: true,
+                        onLoading: function(transport) {
+                                $('installments').innerHTML = '<img src=\"".$this->getSkinUrl('images/opc-ajax-loader.gif')."\" /> <span style=\"color: #888;\">Carregando Parcelas...</span>';
+                        },
+                        onSuccess: function(transport) {
+                              if (200 == transport.status) {
+                                $('installments').innerHTML = transport.responseText;
+                              }
+                },
+                        onFailure: function() {}
+                });
+            }
+      }
+
+      function checkNewToken() {
+          token = document.getElementById('gerencianet_card_token');
+          if (token) {
+              if(token.value) {
+                  var urlAjax = '" . Mage::getUrl("gerencianet/payment/checktoken") . "';
+                  new Ajax.Request( urlAjax, {
+                        method: 'POST',
+                        parameters: 'token=' + token.value,
+                        evalScripts: true,
+                        onSuccess: function(transport) {
+                              if (200 == transport.status) {
+                                 if (transport.responseText == 'FALSE') {
+                                  document.getElementById('gerencianet_card_token').value = '';
+                                    checkToken();
+                                 }
+                              }
+                },
+                        onFailure: function() {}
+                });
+              }
+         }
+      }
+
+      window.onload = function(){
+        var s=document.createElement('script');
+        s.type='text/javascript';
+        var v=parseInt(Math.random()*1000000);
+        s.src='https://".( ($testmode) ? 'sandbox' : 'api' ) . ".gerencianet.com.br/v1/cdn/" . $account_id . "/'+v;
+        s.async=false;
+        s.id='". $account_id . "';
+        if(!document.getElementById('" . $account_id . "')){
+          document.getElementsByTagName('head')[0].appendChild(s);
+        };
+        $"."gn={validForm:true,processed:false,done:{},ready:function(fn){ $"."gn.done=fn; }};
+
+        $"."gn.ready(function(checkout) {
+        });
+
+        if (Validation) {
+            Validation.add('validate-payment-token', 'Dados de cartão inválidos', function (value) {
+                return !!value;
+            });
+        }
+
+        if (Payment) {
+          var saveFn = Payment.prototype.save;
+
+          Payment.prototype.save = function() {
+            var payment = this;
+
+            if (payment.currentMethod == 'gerencianet_card') {
+              document.getElementById('gerencianet_card_token').value = '';
+              var cards = document.getElementsByName('payment[cc_type]');
+              var type;
+              for (i = 0; i < cards.length; i++) {
+                if (cards[i].checked) {
+                  type = cards[i].value;
+                  if (cards[i].value === 'amex') {
+                    document.getElementById('gerencianet_card_cc_cid').setAttribute('maxlength', 4);
+                  } else {
+                    document.getElementById('gerencianet_card_cc_cid').setAttribute('maxlength', 3);
+                  }
+                }
+              }
+              var number    = document.getElementById('gerencianet_card_cc_number').value;
+              var cvv     = document.getElementById('gerencianet_card_cc_cid').value;
+              var exp_month  = document.getElementById('gerencianet_card_cc_expiration').value;
+              var exp_year   = document.getElementById('gerencianet_card_cc_expiration_yr').value;
+
+
+              $"."gn.checkout.getPaymentToken({
+                brand: type,
+                number: number,
+                cvv: cvv,
+                expiration_month: exp_month,
+                expiration_year: exp_year
+              }, function(error, response) {
+
+                if (response && response.code == 200) {
+                  document.getElementById('gerencianet_card_token').value = response.data.payment_token;
+                }
+                saveFn.call(payment)
+              });
+            } else {
+              saveFn.call(payment);
+            }
+          };
+        } else {
+          setInterval(checkNewToken, 4000);
+        }
+      };
+    //]]>
+    </script>");
+
+      $head = Mage::app()->getLayout()->getBlock('after_body_start');
+
+      if($head) {
+        $head->append($jsBlock);
+      }
+
+      return parent::_prepareLayout();
     }
 
 
@@ -212,6 +266,6 @@ class Gerencianet_Transparent_Block_Card_Form extends Mage_Payment_Block_Form_Cc
         return $this->getLayout()->createBlock('payment/form_cc')
                         ->setMethod($this->getMethod());
     }
-    
+
 
 }
